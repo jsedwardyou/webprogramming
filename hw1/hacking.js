d3e52db1282991ba093d0771f95917eca23e7af2
jooseung <jsedwardyou@gmail.com>
2020-04-01 17:03:09 +0900

    code refactoring for text display

--- hw1/hacking.js
+++ hw1/hacking.js
@@ -2,23 +2,20 @@ var input_text = "Hello World!";
 var panel = document.getElementById("panel_text");
 
 var container = document.getElementById("container");
-var notes = [
-  {scenario: 1, intro: "This is the introduction.", que: "What is the weight of ....?"},
-  {scenario: 2, intro: "This is the second scen.", que: "What is the second law of ...?"},
-  {scenario: 3, intro: "This is the third thing.", que: "What is the third law of ...?"}
-];
-function terminal(cl, i) {
+
+function displayText(classname, container, text, speed){
   var div = document.createElement('div');
-  div.className = cl;
+  div.className = classname;
   container.appendChild(div);
-  var txt = [notes[0].intro, notes[0].que].join('\n').split('');
+
   var i = 0;
-  (function display() {
-    if(i < txt.length) {
-      div.innerHTML += txt[i].replace('\n', '<br />');
+  (function display(){
+    if(i < text.length){
+      div.innerHTML += text[i].replace('\n', '<br />');
       ++i;
-      setTimeout(display, 100);
+      setTimeout(display, speed);
     }
   })();
 }
-terminal('blueTh', 0);
+
+displayText("test", container, "Hello", 10);
